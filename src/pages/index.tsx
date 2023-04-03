cd40c2067e309f369571dcc62cd9e0b6f2775ad0
Mercad0 <abimaelmercado05@gmail.com>
2023-04-03 03:56:25 +0000

    removed unnecessary files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,16 @@ import NavBar from "@/components/Navbars/NavBar";
 import Cards from "@/components/Cards";
 import Footer from "@/components/Footer";
 import Address from "@/components/Sections/Address";
+import Head from "next/head";
 
 function Home() {
   return (
     <div className="flex flex-col max-w-fit text-white relative text-center w-full max-h-full mb-30">
+      <Head>
+        <title>Tea Cozy</title>
+        <meta name="description" content="Tea Cozy - Lots of Teas with different flavors! and Donuts" />
+        <link rel="icon" href="/teaIcon.png" />
+      </Head>
       <NavBar />
       <Mission />
       <Cards />
